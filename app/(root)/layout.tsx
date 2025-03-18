5a037479505f3af89929a13a6fe757c7067a6003
Nebiyu Musbah <nebiyumusbah378@gmail.com>
2025-03-18 10:46:50 +0300

    new start

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Navbar from "../components/Navbar";
+
+export default function layout({ children }:{children: React.ReactNode}) {
+  return (
+      
+      <main>
+        <Navbar />
+        {children}
+        </main>
+    
+  );
+}
